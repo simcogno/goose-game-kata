383259fa57b436e31832e1c1bca471991067504d
Simone <simcogno@gmail.com>
2018-05-23 11:04:11 +0200

    Restarted Kata

--- src/main/java/GooseGame.java
+++ src/main/java/GooseGame.java
@@ -1,5 +1,6 @@
+import java.util.*;
 
-public class GooseGameKata {
+public class GooseGame {
 
 
 }
--- src/test/java/GooseGameKataTest.java
+++ src/test/java/GooseGameKataTest.java
@@ -4,8 +4,6 @@ import static org.junit.Assert.*;
 
 public class GooseGameKataTest {
 
-    @Test
-    public void checkIfNoParticipants() {
 
-    }
+
 }
